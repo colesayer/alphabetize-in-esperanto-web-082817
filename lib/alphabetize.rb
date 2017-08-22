fafd37410cfc695b2177e8ba38a63764617f6911
Cole Shapiro <colesayershapiro@gmail.com>
2017-08-22 21:27:34 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+# require 'pry'
+#
+# arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by {|x| x.chars.map { |c| esperanto.index(c) }}
 end
+
+# alphabetize(arr)
